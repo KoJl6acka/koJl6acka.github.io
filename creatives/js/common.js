a7f99f1819514c1c91b7f3c3dddb6b87546862dc
Ruslan Denha <ruslandenha@gmail.com>
2018-07-16 22:15:17 +0300

    go

--- creatives/js/common.js
+++ creatives/js/common.js
@@ -1,5 +1,29 @@
 $(function(){
 
+    var $alertItem = $();
+    var $button = $();
 
+    $button.on('click', function(){
+       var handler = function(){
+           $alertItem.css('display', 'none');
+           $alertItem.removeClass('main_nav_open');
+           $alertItem.off('transitionend', handler);
+       };
+
+
+        $alertItem.addClass('main_nav_open');
+        $alertItem.on('transitionend', handler);
+
+
+
+    });
+
+function raf(fn){
+    window.requestAnimationFrame(function(){
+        window.requestAnimationFrame(function(){
+            fn();
+        })
+    })
+}
 
 });
