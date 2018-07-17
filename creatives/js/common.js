f2be5f48f66d85b7134f0bfaaef5faf19a2e6da2
Ruslan Denha <ruslandenha@gmail.com>
2018-07-17 05:35:14 +0300

    go

--- creatives/js/common.js
+++ creatives/js/common.js
@@ -11,18 +11,38 @@ $(function(){
             $('.main_hamburger').css('width', '60');
             $('.main_hamburger-inner').removeClass('main_hamburger-inner-animate');
             $('nav').removeClass('main_nav_open');
-
-
         }
 
     });
 
-    function raf(fn){
-        window.requestAnimationFrame(function(){
-            window.requestAnimationFrame(function(){
-                fn();
-            })
-        })
+    /*function scrollFunction() {
+        var scroll = $('.pointer').offset().top;
+        var clientHeight = document.documentElement.clientHeight;
+        if(scroll < clientHeight)
+            $('.pointer').css('opacity', 0);
+        else
+            $('.pointer').css('opacity', scroll / (clientHeight * 3));
     }
+
+    scrollFunction();
+
+    $(window).on('scroll', scrollFunction);
+
+    $('.pointer').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000, $.bez([.88,.01,0,.69]));
+    });*/
+
+    $('.main_nav').on('click','a', function (e) {
+        e.preventDefault();
+
+        var selector = $(this).attr('href');
+        var h = $(selector);
+
+        $('html, body').animate({
+            scrollTop: h.offset().top
+        }, 1000, $.bez([.88,.01,0,.69]));
+    });
 
 });
