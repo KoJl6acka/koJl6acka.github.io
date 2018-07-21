ee3863da3f647bcc31c5a8bd92acf84f8497467d
Ruslan Denha <ruslandenha@gmail.com>
2018-07-21 21:28:31 +0300

    go

--- pacman/js/common.js
+++ pacman/js/common.js
@@ -1,39 +1,37 @@
-window.onload = function() {
+window.onload = function () {
 
-/* 1 - box
-   2 - coin
-   3 - ground
- */
+    /* 1 - box
+       2 - coin
+       3 - ground
+     */
     var score = 0;
-    var pressed = false;
+    //var pressed = false;
     var map = [
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,2,2,2,2,2,2,2,2,2,3,2,2,2,2,2,2,2,2,2,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1,1,1,1,2,1],
-        [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
 
     function createWorld() {
         document.getElementById('world').innerHTML = '';
         var elementClass;
-        for(var y = 0; y < map.length; y++)
-        {
-            for(var x = 0; x < map[y].length; x++)
-            {
-                if(map[y][x] === 1){
+        for (var y = 0; y < map.length; y++) {
+            for (var x = 0; x < map[y].length; x++) {
+                if (map[y][x] === 1) {
                     elementClass = 'box';
                 }
-                if(map[y][x] === 2){
+                if (map[y][x] === 2) {
                     elementClass = 'coin';
                 }
-                if(map[y][x] === 3){
+                if (map[y][x] === 3) {
                     elementClass = 'ground';
                 }
                 var div = document.createElement('div');
@@ -52,12 +50,12 @@ window.onload = function() {
         fps: 3,
         stepX: null,
         stepY: null,
-        tryStepX : null,
-        tryStepY : null,
-        run : true,
-        PACMAN : null,
-        move : function (x, y) {
-            if(pacman.run){
+        tryStepX: null,
+        tryStepY: null,
+        run: true,
+        PACMAN: null,
+        move: function (x, y) {
+            if (pacman.run) {
                 var offset, steps, timer;
                 pacman.run = false;
                 pacman.stepX = x;
@@ -65,7 +63,7 @@ window.onload = function() {
                 pacman.PACMAN = document.getElementsByClassName('pacman')[0];
                 pacman.PACMAN.classList.remove('pacman_bottom', 'pacman_top', 'pacman_left');
                 if (pacman.stepX !== 0) {
-                    if (map[pacman.Y + pacman.stepY][pacman.X] !== 1) {
+                    if (map[pacman.Y][pacman.X + pacman.stepX] !== 1) {
                         if (pacman.stepX === -1)
                             pacman.PACMAN.classList.add('pacman_left');
 
@@ -89,6 +87,8 @@ window.onload = function() {
                                 }
                             }
                         }, 30);
+                    } else {
+                        pacman.run = true;
                     }
                 } else if (pacman.stepY !== 0) {
                     if (map[pacman.Y + pacman.stepY][pacman.X] !== 1) {
@@ -98,11 +98,11 @@ window.onload = function() {
 
                         offset = pacman.PACMAN.offsetTop;
                         steps = pacman.cell / pacman.fps;
-                        timer = setInterval(function(){
+                        timer = setInterval(function () {
                             offset += pacman.fps * pacman.stepY;
                             pacman.PACMAN.style.top = offset + 'px';
-                            steps --;
-                            if(steps === 0) {
+                            steps--;
+                            if (steps === 0) {
                                 clearInterval(timer);
                                 pacman.Y = offset / pacman.cell;
                                 pacman.run = true;
@@ -117,12 +117,14 @@ window.onload = function() {
                             }
                         }, 30);
 
+                    } else {
+                        pacman.run = true;
                     }
                 }
                 pacman.eat();
             }
         },
-        eat : function(){
+        eat: function () {
             if (document.getElementById('cell' + pacman.X + pacman.Y).classList.contains('coin')) {
                 score += 10;
                 document.getElementById('cell' + pacman.X + pacman.Y).classList.remove('coin');
@@ -133,35 +135,35 @@ window.onload = function() {
     };
 
 
-    document.onkeydown = function(e) {
-        if (e.keyCode === 37){
-            if(pacman.run)
-                pacman.move(-1,0);
-            else{
+    document.onkeydown = function (e) {
+        if (e.keyCode === 37) {
+            if (pacman.run)
+                pacman.move(-1, 0);
+            else {
                 pacman.tryStepX = -1;
                 pacman.tryStepY = 0;
             }
         }
-        if (e.keyCode === 38){
-            if(pacman.run)
-                pacman.move(0,-1);
-            else{
+        if (e.keyCode === 38) {
+            if (pacman.run)
+                pacman.move(0, -1);
+            else {
                 pacman.tryStepX = 0;
                 pacman.tryStepY = -1;
             }
         }
-        if (e.keyCode === 39){
-            if(pacman.run)
-                pacman.move(1,0);
-            else{
+        if (e.keyCode === 39) {
+            if (pacman.run)
+                pacman.move(1, 0);
+            else {
                 pacman.tryStepX = 1;
                 pacman.tryStepY = 0;
             }
         }
-        if (e.keyCode === 40){
-            if(pacman.run)
-                pacman.move(0,1);
-            else{
+        if (e.keyCode === 40) {
+            if (pacman.run)
+                pacman.move(0, 1);
+            else {
                 pacman.tryStepX = 0;
                 pacman.tryStepY = 1;
             }
